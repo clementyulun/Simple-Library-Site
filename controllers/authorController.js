d088f1e46532be876dca56a5a82ae32286d36428
picarico <picarico@localhost>
2021-09-28 22:20:19 +0800

    feature: add author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,7 @@
 const Author = require('../models/author')
+const Book = require('../models/book')
 const { body, validationResult } = require('express-validator');
+const async = require('async')
 
 // Display list of all Authors.
 exports.author_list = (req, res) => {
@@ -13,7 +15,24 @@ exports.author_list = (req, res) => {
 
 // Display detail page for a specific Author.
 exports.author_detail = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id)
+    async.parallel({
+        author: (callback)=>{
+            Author.findById(req.params.id).exec(callback)
+        },
+        authors_books: (callback)=>{
+            Book.find({author: req.params.id}, 'title summary')
+                .exec(callback)
+        }
+    }, (err, results)=>{
+        if(err) return next(err)
+        if(results.author == null){
+            let err = new Error('Author not found')
+            err.status = 404
+            return next(err)
+        }
+        console.log(results.authors_books)
+        res.render('author_detail', {title: 'Authoe Detail', author: results.author, author_books: results.authors_books})
+    })
 }
 
 // Display Author create form on GET.
