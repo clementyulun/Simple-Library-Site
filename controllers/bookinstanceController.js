f7a17157c8620fd18c900656017f6e0ebf391a6d
picarico <picarico@localhost>
2021-10-01 00:51:37 +0800

    feature: add book instance create page

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,8 @@
 const { nextTick } = require('async')
-const bookinstance = require('../models/bookinstance')
 const BookInstance = require('../models/bookinstance')
+const Book = require('../models/book')
+
+const { body, validationResult } = require('express-validator')
 
 // Display list of all BookInstances.
 exports.bookinstance_list = (req, res) => {
@@ -29,13 +31,45 @@ exports.bookinstance_detail = (req, res) => {
 
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance create GET')
-}
-
-// Handle BookInstance create on POST.
-exports.bookinstance_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance create POST')
-}
+    Book.find({}, "title").exec((err, results) => {
+      if (err) return next(err);
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: results,
+      })
+    })
+  }
+  
+  // Handle BookInstance create on POST.
+  exports.bookinstance_create_post = [
+    body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+    body("imprint", "Imprint must be specified").trim().isLength({ min: 1 }).escape(),
+    body("status").escape(),
+    body("due_back", "Invalid date").optional({ checkFalsy: true }).isISO8601().toDate(),
+    (req, res, next) => {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        Book.find({}, "title").exec((err, results) => {
+          if (err) return next(err)
+          res.render("bookinstance_form", {
+            title: "Create BookInstance",
+            book_list: results,
+          })
+        })
+      } else {
+        let bookinstance = new BookInstance({
+          book: req.body.book,
+          imprint: req.body.imprint,
+          status: req.body.status,
+          due_back: req.body.due_back,
+        })
+        bookinstance.save((err)=>{
+          if(err) return next(err)
+          res.redirect(bookinstance.url)
+        })
+      }
+    }
+  ]
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = (req, res) => {
